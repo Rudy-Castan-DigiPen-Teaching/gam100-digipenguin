14dd75d3b36eea709b15c0c836546e28ee657bb7
hoogoo0118 <gggentooo@gmail.com>
2022-11-09 18:11:24 +0900

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,5 +1,39 @@
-//test
+#if !defined(MAP_H)
+#define MAP_H
 
-#include "map.h"
+constexpr int map_render_area_width = 16;
+constexpr int map_render_area_height = 12;
 
+<<<<<<< Updated upstream
 int Map::render_area
+=======
+<<<<<< < Updated upstream
+	struct Map {
+	int render_area[map_render_area_width][map_render_area_height] = {
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	};
+	====== =
+		class Map {
+		int render_area[map_render_area_height][map_render_area_width] = {
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		};
+
+		public:
+			>>>>>> > Stashed changes
+				void update();
+			void draw();
+	};
+
+#endif
+>>>>>>> Stashed changes
